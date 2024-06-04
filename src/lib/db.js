69a61cff2d506541191854f34a16e94b196935a4
Mercenary <oaishazher@gmail.com>
2024-06-04 13:40:37 +0530

    x

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose'
 
+const uri = process.env.MONGODB_URI
 
 export default async function connect() {
 	if (mongoose.connections[0].readyState) {
 		return;
 	}
 	try {
-		await mongoose.connect('mongodb+srv://admin:FV08jMOzSNBz1eVf@cluster0.vrpea8c.mongodb.net/Snx?retryWrites=true&w=majority&appName=Cluster0');
+		await mongoose.connect(uri);
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log(error);
